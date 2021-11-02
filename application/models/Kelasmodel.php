7f1ff1ac7ed7f7293c49fc1d8cca898b6aa321f7
premanterminal <premanterminal@protonmail.com>
2021-11-02 09:44:23 +0700

    perbaikan model kelas

--- application/models/Kelasmodel.php
+++ application/models/Kelasmodel.php
@@ -1,11 +1,46 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class KelasModel extends CI_Model
+class Kelasmodel extends CI_Model
 {
-    private $_table = "md_kelas_kamar";
-
-    public function ViewKelas()
+    public function get($id = NULL)
     {
-        return $this->db->get($this->_table)->result_array();
+        $this->db->from('md_kelas_kamar');
+        if ($id != NULL) {
+            $this->db->where('id_kelas_kamar', $id);
+        }
+        $query = $this->db->get();
+        return $query;
+    }
+
+    public function del($id)
+    {
+        $this->db
+            ->where('id_kelas_kamar', $id)
+            ->delete('md_kelas_kamar');
+    }
+
+    public function add($post)
+    {
+        $params = [
+            
+            'nama_kelas_kamar' => htmlspecialchars($post['nama_kelas_kamar']),
+            'harga_perhari' => htmlspecialchars($post['harga_perhari'])
+        ];
+
+        $this->db->insert('customers', $params);
+    }
+
+    public function edit($post)
+    {
+        $params = [
+            'nama_kelas_kamar' => htmlspecialchars($post['nama_kelas_kamar']),
+            'harga_perhari' => htmlspecialchars($post['harga_perhari']),
+            'updated' => date('Y-m-d H:i:s')
+        ];
+
+        $this->db
+            ->where('id_kelas_kamar', $post['id'])
+            ->update('md_kelas_kamar', $params);
     }
 }
