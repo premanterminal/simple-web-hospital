0be7302067c651f0ffd0e48a83d2fc304fa1f600
premanterminal <premanterminal@protonmail.com>
2021-11-02 09:41:23 +0700

    perbaikan model customer

--- application/models/Customer_model.php
+++ application/models/Customer_model.php
@@ -1,11 +1,50 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Customer_model extends CI_Model
+class Customers_model extends CI_Model
 {
-    private $_table = "md_pelanggan";
-
-    public function ViewCustomer()
+    public function get($id = NULL)
     {
-        return $this->db->get($this->_table)->result_array();
+        $this->db->from('md_pelanggan');
+        if ($id != NULL) {
+            $this->db->where('id_pelanggan', $id);
+        }
+        $query = $this->db->get();
+        return $query;
+    }
+
+    public function del($id)
+    {
+        $this->db
+            ->where('id_pelanggan', $id)
+            ->delete('md_pelanggan');
+    }
+
+    public function add($post)
+    {
+        $params = [
+            
+            'nama_pelanggan' => htmlspecialchars($post['nama_pelanggan']),
+            'tanggal_lahir' => htmlspecialchars($post['tanggal_lahir']),
+            'no_handphone' => htmlspecialchars($post['no_handphone']),
+            'email' => htmlspecialchars($post['email'])
+        ];
+
+        $this->db->insert('customers', $params);
+    }
+
+    public function edit($post)
+    {
+        $params = [
+            'nama_pelanggan' => htmlspecialchars($post['nama_pelanggan']),
+            'tanggal_lahir' => htmlspecialchars($post['tanggal_lahir']),
+            'no_handphone' => htmlspecialchars($post['no_handphone']),
+            'email' => htmlspecialchars($post['email']),
+            'updated' => date('Y-m-d H:i:s')
+        ];
+
+        $this->db
+            ->where('id_pelanggan', $post['id'])
+            ->update('md_pelanggan', $params);
     }
 }
